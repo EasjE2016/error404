8f418b558130bd6e98cf085ce32511e9627a5681
TobiasSS1 <tss3331@hotmail.com>
2016-11-29 14:19:19 +0100

    Har lavet Tilmeldings delen

---
+++ kodning/ViewModel/RemoveTilmeld.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace kodning.ViewModel
+{
+    public class RemoveTilmeld : ICommand
+    {
+        private readonly Action execute;
+        public event EventHandler CanExecuteChanged;
+
+        public RemoveTilmeld(Action execute)
+        {
+            this.execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+    }
+}
