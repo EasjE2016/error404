bd58f9dae81654ed06f4e382c7a753a9783e1ec5
TobiasSS1 <tss3331@hotmail.com>
2016-12-02 11:40:25 +0100

    Har bindet og lagt op tl at man kan redigere og oprette en madplan

--- kodning/EditMadplan.xaml.cs
+++ kodning/EditMadplan.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,23 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace kodning
+namespace kodning.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Forside: Page
     {
-        public MainPage()
+        public Forside()
         {
             this.InitializeComponent();
-        }
-
-        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
         }
     }
 }
