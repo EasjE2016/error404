74fa65e4c7c600a57d818056f4892f12c6e5b6e8
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2016-12-07 13:12:32 +0100

    Startet på dagslister for kuverter

---
+++ kodning/Model/UgeKuverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kodning.Model
+{
+    class UgeKuverter
+    {
+        List<int> KuvertMandag = new List<int>();
+
+        List<int> KuvertTirsdag = new List<int>();
+
+        List<int> KuvertOnsdag = new List<int>();
+
+        List<int> KuvertTorsdag = new List<int>();
+    }
+}
