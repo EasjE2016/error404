ee0fdadf5bb5ace746929124c9b0918ca5dbc22d
TobiasSS1 <tss3331@hotmail.com>
2016-12-13 14:29:59 +0100

    Har lavet SekvensDiagram

--- kodning/ViewModel/KuvertViewModel.cs
+++ kodning/ViewModel/KuvertViewModel.cs
@@ -75,7 +75,7 @@ namespace kodning.ViewModel
             
         }
         public void BeregnKuvertMandag()
-        {
+                    {
             PrisBeregning.MandagTotalKuvert = GivAlleKuverterMandag;
         }
 
