e12248a0549b1d569be3ce7adc67e437c86ae4ab
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2016-12-15 11:59:56 +0100

    Ændret siden 'Tobias.xaml' til 'ChangeMenu.xaml'

--- kodning/View/ChangeMenu.xaml.cs
+++ kodning/View/ChangeMenu.xaml.cs
@@ -20,9 +20,9 @@ namespace kodning.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Tobias : Page
+    public sealed partial class ChangeMenu : Page
     {
-        public Tobias()
+        public ChangeMenu()
         {
             this.InitializeComponent();
         }
--- kodning/View/Tilmeld.xaml.cs
+++ kodning/View/Tilmeld.xaml.cs
@@ -41,5 +41,10 @@ namespace kodning.View
         {
             this.Frame.Navigate(typeof(Forside), null);
         }
+
+        private void ChangeMenuClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(kodning.View.ChangeMenu), null);
+        }
     }
 }
