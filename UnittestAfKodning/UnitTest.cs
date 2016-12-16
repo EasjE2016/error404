ddd4ba331802d4b1778820f3d5dc57b00739342b
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2016-12-16 12:37:37 +0100

    Ændert view til madplansændring

---
+++ UnittestAfKodning/UnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using kodning.ViewModel;
+
+namespace UnittestAfKodning
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private KuvertViewModel kvm;
+
+        [TestInitialize]
+        public void FørHverTest()
+        {
+            kvm = new KuvertViewModel();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            kvm.AddNewKuvertMandag();
+        }
+    }
+}
