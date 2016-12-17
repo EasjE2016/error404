4c0925b04c343135bb82b5ed1c61de5735e7f53b
Kasper <Kasper@DESKTOP-QG1AVPG>
2016-12-17 12:42:12 +0100

    Har tilføjet koden til at rydde liste over deltagere der skal bare tilføjes en knap til den + jeg har ryddet op i kuvertviewmodel

--- kodning/ViewModel/KuvertViewModel.cs
+++ kodning/ViewModel/KuvertViewModel.cs
@@ -1,28 +1,27 @@
-﻿using System;
+﻿using kodning.Model;
+using kodning.RelayCommand;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using kodning.Model;
-using System.ComponentModel;
 using Windows.Storage;
 using Windows.UI.Popups;
-using kodning.RelayCommand;
-using Newtonsoft.Json;
 
 namespace kodning.ViewModel
 {
     public class KuvertViewModel : NotifyPropNotifications
     {
         public Kuverter Kuverter { get; set; }
-        //public UgeKuverter UgeKuverter { get; set; }
+
         public PrisBeregning PrisBeregning { get; set; }
-        public string Kuvertfilnavn { get; private set; }
-        //private KuverterListe _kuvertsliste;
+
         public KuvertCatalogSingleton Instance { get; set; }
 
 
-        #region Foreach loop over samlet antal kuverter
+        #region Foreach loop over kuverter
 
         public double GivAlleKuverterMandag
         {
@@ -53,9 +52,8 @@ namespace kodning.ViewModel
             }
         }
 
-        // Dette SKAL ordnes. 
 
-        public double PrisPerKuvertTest
+        public double PrisPerKuvert
         {
             set
             {
@@ -65,160 +63,7 @@ namespace kodning.ViewModel
             { return PrisBeregning.Pris; }
         }
 
-
-
-
-        #endregion
-
-
-        #region RelayCommands
-
-        public RelayCommand.RelayCommand TilmeldAlleCommand { get; set; }
-        public RelayCommand.RelayCommand TilmeldCommand { get; set; }
-        public RelayCommand.RelayCommand KuvertPerDagCommand { get; set; }
-        public RelayCommand.RelayCommand BeregnKuvMandag { get; set; }
-        public RelayCommand.RelayCommand TilMeldTirsdagCommand { get; set; }
-        public RelayCommand.RelayCommand TilMeldOnsdagCommand { get; set; }
-        public RelayCommand.RelayCommand TilMeldTorsdagCommand { get; set; }
-        public RelayCommand.RelayCommand AccepterUdlægCommand { get; set; }
-        #endregion
-
-
-        #region Metode til at fortælle hvorvidt der er sket en ændring propertychange
-        //public event PropertyChangedEventHandler PropertyChanged;
-        //protected virtual void OnPropertyChanged(string propertyName)
-        //{
-        //    if (PropertyChanged != null)
-        //    {
-        //        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-        //    }
-        //}
-        #endregion
-
-        //public PrisBeregning Pris
-        //{
-        //    get
-        //    {
-        //        return _Pris;
-        //    }
-        //    set
-        //    {
-        //        _Pris = value;
-        //        OnPropertyChanged(nameof(Pris));
-        //    }
-        //}
-        #region onpropchange til KuvertLister
-        //public KuverterListe KuvertListenMandag
-        //{
-        //    get { return _kuvertsliste; }
-        //    set
-        //    {
-        //        _kuvertsliste = value;
-        //        OnPropertyChanged(nameof(KuvertListenMandag));
-        //        //OnPropertyChanged(nameof(GivAlleKuverterMandag));S
-        //    }
-        //}
-
-        //public KuverterListe KuvertListenTirsdag
-        //{
-        //    get { return _kuvertsliste; }
-        //    set
-        //    {
-        //        _kuvertsliste = value;
-        //        OnPropertyChanged(nameof(KuvertListenTirsdag));
-        //    }
-        //}
-        //public KuverterListe KuvertListenOnsdag
-        //{
-        //    get { return _kuvertsliste; }
-        //    set
-        //    {
-        //        _kuvertsliste = value;
-        //        OnPropertyChanged(nameof(KuvertListenOnsdag));
-        //    }
-        //}
-        //public KuverterListe KuvertListenTorsdag
-        //{
-        //    get { return _kuvertsliste; }
-        //    set
-        //    {
-        //        _kuvertsliste = value;
-        //        OnPropertyChanged(nameof(KuvertListenTorsdag));
-        //    }
-        //}
-
-
-
-
-
-
-
-        #endregion
-
-
-
-        #region Metode til Tilføje Kuverter
-        //public void AccepterUdlæg()
-        //{
-        //    PrisBeregning Udlæg = new PrisBeregning();
-        //    Udlæg.Kok1Udlæg = PrisBeregning.Kok1Udlæg;
-        //    Udlæg.Kok2Udlæg = PrisBeregning.Kok2Udlæg;
-        //    Udlæg.Kok3Udlæg = PrisBeregning.Kok3Udlæg;
-        //    Udlæg.Kok4Udlæg = PrisBeregning.Kok4Udlæg;
-        //    Udlæg.UdlægIAlt = PrisBeregning.UdlægIAlt;
-        //}
-
-        public void AddNewKuvertMandag()
-        {
-            Kuverter Kuvert = new Kuverter();
-            Kuvert.Husnummer = Kuverter.Husnummer;
-            Kuvert.Voksne = Kuverter.Voksne;
-            Kuvert.Teens = Kuverter.Teens;
-            Kuvert.Boern = Kuverter.Boern;
-            Kuvert.Baby = Kuverter.Baby;
-            Kuvert.Ugedag = "";
-
-            //referer til singleton
-            Instance.MandagListe.Add(Kuvert);
-
-        }
-        public void AddNewKuvertTirsdag()
-        {
-            Kuverter TirsdagKuvert = new Kuverter();
-            TirsdagKuvert.Husnummer = Kuverter.Husnummer;
-            TirsdagKuvert.TirsdagVoksne = Kuverter.TirsdagVoksne;
-            TirsdagKuvert.TirsdagTeens = Kuverter.TirsdagTeens;
-            TirsdagKuvert.TirsdagBoern = Kuverter.TirsdagBoern;
-            TirsdagKuvert.TirsdagBaby = Kuverter.TirsdagBaby;
-            //referer til singleton
-            Instance.TirsdagListe.Add(TirsdagKuvert);
-
-        }
-        public void AddNewKuvertOnsdag()
-        {
-            Kuverter OnsdagKuvert = new Kuverter();
-            OnsdagKuvert.Husnummer = Kuverter.Husnummer;
-            OnsdagKuvert.OnsdagVoksne = Kuverter.OnsdagVoksne;
-            OnsdagKuvert.OnsdagTeens = Kuverter.OnsdagTeens;
-            OnsdagKuvert.OnsdagBoern = Kuverter.OnsdagBoern;
-            OnsdagKuvert.OndagsBaby = Kuverter.OndagsBaby;
-            //referer til singleton
-            Instance.OnsdagListe.Add(OnsdagKuvert);
-
-        }
-        public void AddNewKuvertTorsdag()
-        {
-            Kuverter TorsdagKuvert = new Kuverter();
-            TorsdagKuvert.Husnummer = Kuverter.Husnummer;
-            TorsdagKuvert.TorsdagVoksne = Kuverter.TorsdagVoksne;
-            TorsdagKuvert.TorsdagTeens = Kuverter.TorsdagTeens;
-            TorsdagKuvert.TorsdagBoern = Kuverter.TorsdagBoern;
-            TorsdagKuvert.TorsdagBaby = Kuverter.TorsdagBaby;
-            //referer til singleton
-            Instance.TorsdagListe.Add(TorsdagKuvert);
-
-        }
-        public void prisErTest()
+        public void prisPerKuvertEr()
         {
             PrisBeregning Udlæg = new PrisBeregning();
             Udlæg.Kok1Udlæg = PrisBeregning.Kok1Udlæg;
@@ -226,21 +71,25 @@ namespace kodning.ViewModel
             Udlæg.Kok3Udlæg = PrisBeregning.Kok3Udlæg;
             Udlæg.Kok4Udlæg = PrisBeregning.Kok4Udlæg;
             Udlæg.UdlægIAlt = PrisBeregning.UdlægIAlt;
-            PrisPerKuvertTest = PrisPerKuvertTest;
+            PrisPerKuvert = PrisPerKuvert;
 
         }
+
+
+        #endregion
+
+        #region RelayCommands
+
+        public RelayCommand.RelayCommand TilmeldAlleCommand { get; set; }
+        public RelayCommand.RelayCommand KuvertPerDagCommand { get; set; }
+        public RelayCommand.RelayCommand RydListeOverDeltagere { get; set; }
+
+
+        #endregion
+
         #region Metode med IF statements der tilføjer kuverter til listen
         public void AddAlleDage()
         {
-            //if (Kuverter.Voksne == 0 && Kuverter.Teens == 0 && Kuverter.Boern == 0 && Kuverter.Baby == 0)
-            //{
-            //    // her skal der tilføjes en exception - Måske Try catch? catch (System.IndexOutOfRangeException ex)
-            //    //{
-            //    //    System.ArgumentException argEx = new System.ArgumentException("noob text her ", ex);
-            //    //    throw argEx;
-            //    //}
-            //}
-
             bool erNulTilmeldte = true;
 
             if (Kuverter.Husnummer != 0)
@@ -262,11 +111,6 @@ namespace kodning.ViewModel
 
                 }
 
-                //if (Kuverter.TirsdagVoksne == 0 && Kuverter.TirsdagTeens == 0 && Kuverter.TirsdagBaby == 0 && Kuverter.TirsdagBaby == 0)
-                //{
-
-                //}
-
                 if (Kuverter.TirsdagVoksne > 0 || Kuverter.TirsdagTeens > 0 || Kuverter.TirsdagBaby > 0 || Kuverter.TirsdagBaby > 0)
                 {
                     erNulTilmeldte = false;
@@ -282,13 +126,6 @@ namespace kodning.ViewModel
 
                 }
 
-
-
-                //if (Kuverter.OnsdagVoksne == 0 && Kuverter.OnsdagTeens == 0 && Kuverter.OnsdagBoern == 0 && Kuverter.OndagsBaby == 0)
-                //{
-
-                //}
-
                 if (Kuverter.OnsdagVoksne > 0 || Kuverter.OnsdagTeens > 0 || Kuverter.OnsdagBoern > 0 || Kuverter.OndagsBaby > 0)
                 {
                     erNulTilmeldte = false;
@@ -303,11 +140,6 @@ namespace kodning.ViewModel
                     Instance.OnsdagListe.Add(OnsdagKuvert);
 
                 }
-
-                //if (Kuverter.TorsdagVoksne == 0 && Kuverter.TorsdagTeens == 0 && Kuverter.TorsdagBoern == 0 && Kuverter.TorsdagBaby == 0)
-                //{
-
-                //}
 
                 if (Kuverter.TorsdagVoksne > 0 || Kuverter.TorsdagTeens > 0 || Kuverter.TorsdagBoern > 0 || Kuverter.TorsdagBaby > 0)
                 {
@@ -342,6 +174,15 @@ namespace kodning.ViewModel
         }
         #endregion
 
+        #region
+        public void StartNyUge()
+        {
+            Instance.MandagListe.Clear();
+            Instance.TirsdagListe.Clear();
+            Instance.OnsdagListe.Clear();
+            Instance.TorsdagListe.Clear();
+            this.GemDataTilDiskAsync();
+        }
         #endregion
 
         #region Konstruktør
@@ -351,20 +192,15 @@ namespace kodning.ViewModel
             PrisBeregning = new PrisBeregning();
             Kuverter = new Kuverter();
             TilmeldAlleCommand = new RelayCommand.RelayCommand(AddAlleDage);
-            TilmeldCommand = new RelayCommand.RelayCommand(AddNewKuvertMandag);
-            TilMeldTirsdagCommand = new RelayCommand.RelayCommand(AddNewKuvertTirsdag);
-            TilMeldOnsdagCommand = new RelayCommand.RelayCommand(AddNewKuvertOnsdag);
-            TilMeldTorsdagCommand = new RelayCommand.RelayCommand(AddNewKuvertTorsdag);
-            //AccepterUdlægCommand = new RelayCommand.RelayCommand(AccepterUdlæg);
-            KuvertPerDagCommand = new RelayCommand.RelayCommand(prisErTest);
-
+            KuvertPerDagCommand = new RelayCommand.RelayCommand(prisPerKuvertEr);
+            RydListeOverDeltagere = new RelayCommand.RelayCommand(StartNyUge);
+            
             this.HentDataFraDiskAsync();
         }
         #endregion
 
         #region Json
 
-        StorageFolder localfolder = null;
         public async void GemDataTilDiskAsync()
         {
             string jsonText = Instance.MandagListe.SaveJson();
@@ -413,7 +249,6 @@ namespace kodning.ViewModel
                 //Popup vindue til at fortælle brugeren at filen ikke blev fundet. 
                 MessageDialog messageDialog = new MessageDialog(ex.Message,"Filen ikke fundet");
                 await messageDialog.ShowAsync();
-                //throw;
             }
         }
         #endregion
