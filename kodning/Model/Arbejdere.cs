eeef0ffb65c6a2f40ac9d4c6a226dd6538225e67
Kasper <Kasper@DESKTOP-QG1AVPG>
2016-12-17 18:01:28 +0100

    Har fixet Arbejder klassen så den virker...

---
+++ kodning/Model/Arbejdere.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kodning.Model;
+
+namespace kodning.Model
+{
+    public class Arbejdere
+    {
+        public string Title { get; set; }
+        public string Navn { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Navn)}: {Navn}, {nameof(Title)}: {Title}";
+        }
+
+
+
+    }
+}
