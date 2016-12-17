8878daafb27cf1e55e52f400133562e28964acff
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2016-12-17 14:54:57 +0100

    Tilføjet view til AddWorker

---
+++ kodning/View/AddWorker.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace kodning.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AddWorker : Page
+    {
+        public AddWorker()
+        {
+            this.InitializeComponent();
+        }
+
+        private void GåTilForside_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Forside), null);
+        }
+
+        private void ErDuKokSide(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ErDuKok), null);
+        }
+
+        private void ChangeMenuClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ChangeMenu), null);
+        }
+    }
+}
